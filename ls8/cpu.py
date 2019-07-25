19eeb048842f6119246623c2a6cba29f923ff84c
Jake Thomas <jmaxt12@gmail.com>
2019-07-25 15:29:53 -0600

    cleaned code and started branch table

--- ls8/cpu.py
+++ ls8/cpu.py
@@ -1,12 +1,14 @@
  # Place where instructions are processed
 """CPU functionality."""
 import sys
+
 LDI = 0b10000010
-HLT = 0b00000001
 PRN = 0b01000111
+HLT = 0b00000001
 MUL = 0b10100010
 PUSH = 0b01000101
 POP = 0b01000110
+
 class CPU:
     """Main CPU class."""
     def __init__(self):
@@ -15,20 +17,25 @@ class CPU:
         self.reg = [0] * 8
         self.pc = 0
         self.SP = 7
+        self.branch_table = {}
+        self.branch_table[LDI] = self.handle_LDI
+        self.branch_table[PRN] = self.handle_PRN
+        self.branch_table[HLT] = self.handle_HLT
+        self.branch_table[MUL] = self.handle_MUL
+        self.branch_table[PUSH] = self.handle_PUSH
+        self.branch_table[POP] = self.handle_POP
         
     def __str__(self):
         return f"RAM: {self.ram}, REGISTER: {self.reg}, PC: {self.pc}"
     
-    # MAR = address; _Memory Address Register_
     def ram_read(self, address):
         return self.ram[address]
     
-    # MDR = value; _Memory Data Register_
     def ram_write(self, value, address):
         self.ram[address] = value
+        
     def load(self):
         """Load a program into memory."""
-        # program = [0] * 256
         if len(sys.argv) != 2:
             print(f"Error: Proper Usage = {sys.argv[0]} filename")
             sys.exit(1)
@@ -43,13 +50,12 @@ class CPU:
                     if num.strip() == '':  # ignores comment-only lines
                         continue
                     self.ram[address] = int(num, 2)
-                    #print(num)
                     address += 1
-           # print(self.ram)
                     
         except FileNotFoundError:
             print(f"{sys.argv[0]}: {sys.argv[1]} not found")
             sys.exit(2)
+            
     def alu(self, op, reg_a, reg_b):
         """ALU operations."""
         if op == "ADD":
@@ -57,6 +63,7 @@ class CPU:
         #elif op == "SUB": etc
         else:
             raise Exception("Unsupported ALU operation")
+          
     def trace(self):
         """
         Handy function to print out the CPU state. You might want to call this
@@ -72,15 +79,35 @@ class CPU:
         ), end='')
         for i in range(8):
             print(" %02X" % self.reg[i], end='')
+            
+    def handle_LDI(self, operand_a, operand_b):
+        pass
+
+    def handle_PRN(self, operand_a):
+        pass
+      
+    def handle_HLT(self):
+        pass
+
+    def handle_MUL(self, operand_a, operand_b):
+        pass
+    
+    def handle_PUSH(self, operand_a):
+        pass
+
+    def handle_POP(self, operand_a):
+        pass
+            
     def run(self):
         """Run the CPU."""
         # determines whether or not this function is "running"
         running = True
+        
+        # SP pointing at 244 in RAM
         self.reg[self.SP] = 244
-        # IR = _Instruction Register_
-        
-        
+      
         while (running):
+          	# IR = _Instruction Register_
             IR = self.ram_read(self.pc)
             
             command = self.ram[self.pc]
@@ -90,39 +117,32 @@ class CPU:
             operand_a = self.ram_read(self.pc + 1)
             operand_b = self.ram_read(self.pc + 2)
                 
-            # LDI = 0b10000010
             if command == LDI:
-                # print('LDI', num_of_ops)
                 self.reg[operand_a] = operand_b
-            
-            # PRN = 0b01000111
-            elif command == PRN:
-                # print('PRN', num_of_ops)    
+
+            elif command == PRN: 
                 print(self.reg[operand_a])
                
-            # HLT = 0b00000001
             elif command == HLT: 
-                # print('HLT', num_of_ops)
                 running = False
-
+                
             elif command == MUL:
-                # print("MUL")
                 self.reg[operand_a] = self.reg[operand_a] * self.reg[operand_b]
-
+                
             elif command == PUSH:
                 self.reg[self.SP] -= 1
                 regnum = self.ram[self.pc + 1]
                 value = self.reg[regnum]
                 self.ram[self.reg[self.SP]] = value
-
+                
             elif command == POP:
                 value = self.ram[self.reg[self.SP]]
                 regnum = self.ram[self.pc + 1]
                 self.reg[regnum] = value
                 self.reg[self.SP] += 1
-
+                
             else: 
                 print(f"unknown instruction: {command}")
                 sys.exit(1)
-            
+                
             self.pc += num_of_ops
